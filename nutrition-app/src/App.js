98bfcc350238652cf64f326de0018286ff127965
farkza <farkzapro@gmail.com>
2024-10-09 10:10:07 +0200

    updated files

--- nutrition-app/src/App.js
+++ nutrition-app/src/App.js
@@ -5,9 +5,6 @@ import AppRouter from './Router';
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Nutrition App</h1>
-      </header>
       <main>
         <AppRouter />
       </main>
